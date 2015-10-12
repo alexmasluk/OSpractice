dd5fca1efa39dfd70c06da57be6448b1569b6d79
alexmasluk <alex.masluk@gmail.com>
2015-10-12 10:37:18 -0700

    Homework 1

---
+++ parallel.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sys/wait.h>
+using namespace std;
+
+int main()
+{
+	ifstream url_file;
+	url_file.open("urls.txt");
+	int count=0;
+	string temp;
+
+	while (getline(url_file, temp))
+		count++;
+	cout << count << endl;
+	url_file.close();
+	string* urls = new string[count];
+	count = 0;
+	
+	url_file.open("urls.txt");
+	while (getline(url_file, temp))
+		urls[count++] = temp;	
+
+	url_file.close();
+	pid_t pid;
+	int n=0;
+	while (n < count)
+	{
+		pid = fork();
+		if (pid < 0) perror("fork error!");
+		else if (pid==0)
+		{
+			execlp("wget","wget", urls[n].c_str(), NULL);
+			_exit(1);
+		}
+		else
+			n++;
+	}
+
+	int status=0;	
+	for (int i=0; i<count; i++)
+		wait(&status);
+	delete[] urls;
+	return 0;
+
+}
